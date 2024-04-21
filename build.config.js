c7890610ee7c961864768bcb15f29a3c8e995577
Kamil <lekamilthemage@gmail.com>
2024-04-21 15:16:51 +0200

    Setup Vite

---
+++ build.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue': 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js',
+    }
+  },
+  base: "/open-source-gallery/",
+})
