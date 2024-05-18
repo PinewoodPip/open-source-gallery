898a1b7aa3970d984824448476dba46ebadade77
Kamil <lekamilthemage@gmail.com>
2024-05-18 10:20:41 +0200

    Update repo list, show stars and collaborator counts

--- public/components/card.js
+++ public/components/card.js
@@ -4,9 +4,16 @@ export default {
     return {};
   },
   methods: {
+    getSuffixedValue(value) {
+      let str = value
+      if (value >= 1000) {
+        str = (value / 1000).toFixed(1) + "K"
+      }
+      return str
+    },
     getRepositoryIcon() {
       return this.repo.icon ?? ""
-    }
+    },
   },
   props: ["repo"],
   template: "#card",
