9bc5acf5d79efd029f46d6c4b957f63579206520
Kamil <lekamilthemage@gmail.com>
2024-06-09 22:56:50 +0200

    Add results to statistics page

--- public/statistics.js
+++ public/statistics.js
@@ -3,77 +3,103 @@ import { createApp } from 'vue'
 import FooterComponent from './components/pageFooter.js'
 import StatContainerComponent from './components/statContainer.js'
 
+const BACKGROUND_COLORS = [
+  'rgba(255, 99, 132, 0.2)',
+  'rgba(255, 159, 64, 0.2)',
+  'rgba(255, 205, 86, 0.2)',
+  'rgba(75, 192, 192, 0.2)',
+  'rgba(54, 162, 235, 0.2)',
+  'rgba(153, 102, 255, 0.2)',
+  'rgba(201, 203, 207, 0.2)'
+]
+
+const BORDER_COLORS = [
+  'rgb(255, 99, 132)',
+  'rgb(255, 159, 64)',
+  'rgb(255, 205, 86)',
+  'rgb(75, 192, 192)',
+  'rgb(54, 162, 235)',
+  'rgb(153, 102, 255)',
+  'rgb(201, 203, 207)'
+]
+
 const app = createApp({
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    setupLanguagesChart() {
+      let ctx = document.getElementById('languages');
+      const labels = ["TypeScript", "JavaScript", "Go", "Python", "Java", "C++", "C#", "Shell", "PHP", "Rust", "C", "Vue", "HTML", "Ruby", "Swift", "Kotlin", "CSS", "SCSS"];
+      const data = [232, 173, 94, 90, 70, 42, 40, 28, 27, 24, 21, 19, 13, 11, 9, 8, 6, 4];
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Repositories',
+            data: data,
+            backgroundColor: BACKGROUND_COLORS,
+            borderColor: BORDER_COLORS,
+            borderWidth: 1,
+          }]
+        },
+        options: {
+          indexAxis: 'y',
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: "Most-used languages in the dataset",
+            }
+          },
+        },
+      });
+    },
+    setupTopicsChart() {
+      let ctx = document.getElementById('topics');
+      const labels = ["hacktoberfest", "javascript", "react", "typescript", "nodejs", "python", "docker", "nextjs", "golang", "go", "minecraft", "git", "java", "vue", "machine-learning", "android", "deep-learning", "github",];
+      const data = [216, 200, 180, 139, 110, 93, 88, 86, 83, 80, 79, 79, 79, 72, 64, 62, 54, 53,];
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Repositories',
+            data: data,
+            backgroundColor: BACKGROUND_COLORS,
+            borderColor: BORDER_COLORS,
+            borderWidth: 1,
+          }]
+        },
+        options: {
+          indexAxis: 'y',
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: "Most-used topics/tags in the dataset",
+            }
+          },
+        },
+      });
+    },
+  },
   components: {
     "statcontainer": StatContainerComponent,
     "pagefooter": FooterComponent,
   },
   mounted() {
-    // Initialize charts
-    let ctx = document.getElementById('testchart1');
-    new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      },
-    });
-
-    ctx = document.getElementById('testchart2');
-    const labels = ["1", "2", "3", "4", "5", "6", "7"];
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'My First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      },
-    });
+    this.setupLanguagesChart();
+    this.setupTopicsChart();
   }
 });
 app.mount("#app");
